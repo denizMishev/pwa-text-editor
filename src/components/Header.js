3444569a67d5b64324759238a380d1aa924bd686
Mishev Deniz <denizmishev@yahoo.com>
2024-02-13 18:13:15 +0200

    added css variables, header component HTML&CSS

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+export function Header() {
+  return (
+    <header id="header">
+      <div className="header-btns-ctr">
+        <button>
+          <i className="fa-solid fa-bold"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-italic"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-underline"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-align-left"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-align-justify"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-align-right"></i>
+        </button>
+      </div>
+    </header>
+  );
+}
